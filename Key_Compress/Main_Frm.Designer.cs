cee205b7fae24347781a28a383aba486fa7e91cf
Asef <asefg482@gmail.com>
2022-02-20 00:23:14 +0330

    Main Commit

---
+++ Key_Compress/Main_Frm.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Key_Compress
+{
+    partial class Main_Frm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Btn_Start = new System.Windows.Forms.Button();
+            this.Btn_Stop = new System.Windows.Forms.Button();
+            this.Tmr_Main = new System.Windows.Forms.Timer(this.components);
+            this.Ch_Top_Most = new System.Windows.Forms.CheckBox();
+            this.Tik_Lbl = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_Start
+            // 
+            this.Btn_Start.Location = new System.Drawing.Point(11, 12);
+            this.Btn_Start.Name = "Btn_Start";
+            this.Btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Start.TabIndex = 0;
+            this.Btn_Start.Text = "Start";
+            this.Btn_Start.UseVisualStyleBackColor = true;
+            this.Btn_Start.Click += new System.EventHandler(this.Btn_Start_Click);
+            // 
+            // Btn_Stop
+            // 
+            this.Btn_Stop.Location = new System.Drawing.Point(92, 12);
+            this.Btn_Stop.Name = "Btn_Stop";
+            this.Btn_Stop.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Stop.TabIndex = 1;
+            this.Btn_Stop.Text = "Stop";
+            this.Btn_Stop.UseVisualStyleBackColor = true;
+            this.Btn_Stop.Click += new System.EventHandler(this.Btn_Stop_Click);
+            // 
+            // Tmr_Main
+            // 
+            this.Tmr_Main.Interval = 1000;
+            this.Tmr_Main.Tick += new System.EventHandler(this.Tmr_Main_Tick);
+            // 
+            // Ch_Top_Most
+            // 
+            this.Ch_Top_Most.AutoSize = true;
+            this.Ch_Top_Most.Location = new System.Drawing.Point(173, 16);
+            this.Ch_Top_Most.Name = "Ch_Top_Most";
+            this.Ch_Top_Most.Size = new System.Drawing.Size(73, 17);
+            this.Ch_Top_Most.TabIndex = 2;
+            this.Ch_Top_Most.Text = "Top Mast";
+            this.Ch_Top_Most.UseVisualStyleBackColor = true;
+            this.Ch_Top_Most.CheckedChanged += new System.EventHandler(this.Ch_Top_Most_CheckedChanged);
+            // 
+            // Tik_Lbl
+            // 
+            this.Tik_Lbl.AutoSize = true;
+            this.Tik_Lbl.Location = new System.Drawing.Point(13, 44);
+            this.Tik_Lbl.Name = "Tik_Lbl";
+            this.Tik_Lbl.Size = new System.Drawing.Size(66, 13);
+            this.Tik_Lbl.TabIndex = 4;
+            this.Tik_Lbl.Text = "Min Line : 0";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(71, 83);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Line Max : ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(197, 85);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Top Mast";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Max Line : 0";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(197, 131);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(73, 17);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "Top Mast";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Thread Sleep : ";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(92, 129);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 9;
+            // 
+            // Main_Frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 183);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Tik_Lbl);
+            this.Controls.Add(this.Ch_Top_Most);
+            this.Controls.Add(this.Btn_Stop);
+            this.Controls.Add(this.Btn_Start);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main_Frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Code Compress";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Start;
+        private System.Windows.Forms.Button Btn_Stop;
+        private System.Windows.Forms.Timer Tmr_Main;
+        private System.Windows.Forms.CheckBox Ch_Top_Most;
+        private System.Windows.Forms.Label Tik_Lbl;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ Key_Compress/Main_Frm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Key_Compress
+{
+    public partial class Main_Frm : Form
+    {
+        public Main_Frm()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Start_Click(object sender, EventArgs e)
+        {
+            Tmr_Main.Start();
+        }
+
+        private void Btn_Stop_Click(object sender, EventArgs e)
+        {
+            Tmr_Main.Stop();
+        }
+
+        int Interval = 0;
+
+        private void Tmr_Main_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send("{HOME}");
+            System.Threading.Thread.Sleep(300);
+            SendKeys.Send("+{UP}");
+            System.Threading.Thread.Sleep(300);
+            SendKeys.Send("+{END}");
+            System.Threading.Thread.Sleep(300);
+            SendKeys.Send("{BACKSPACE}");
+            Interval++;
+            Tik_Lbl.Text = "Min Line : " + Interval;
+        }
+
+        private void Ch_Top_Most_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = (!this.TopMost);
+        }
+    }
+}
